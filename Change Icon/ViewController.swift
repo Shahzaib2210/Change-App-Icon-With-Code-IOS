32b9a8b1da6dba96aad0c98fd3e46cd84ea05b45
Shahzaib Mumtaz <shahzaib@Sofia-Sadiq.localdomain>
2022-04-06 14:45:41 +0500

    First Commit

---
+++ Change Icon/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Change Icon
+//
+//  Created by Shahzaib Mumtaz on 05/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func btnLightAction(_ sender: UIButton) {
+        DispatchQueue.main.async {
+            self.changeIcon(to: "Light")
+        }
+    }
+    
+    @IBAction func btnDarkAction(_ sender: UIButton) {
+        DispatchQueue.main.async {
+            self.changeIcon(to: "Dark")
+        }
+    }
+    
+    func changeIcon(to name: String?) {
+        // Check if the app supports alternating icons
+        guard UIApplication.shared.supportsAlternateIcons else {
+            return;
+        }
+        
+        // Change the icon to a specific image with given name
+        UIApplication.shared.setAlternateIconName(name) { (error) in
+            // After app icon changed, print our error or success message
+            if let error = error {
+                print("App icon failed to due to \(error.localizedDescription)")
+            } else {
+                print("App icon changed successfully.")
+            }
+        }
+    }
+}
+
